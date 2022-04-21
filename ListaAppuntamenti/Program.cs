2c3c343b4c16d177ecd59a5e574db2b0eec71dbf
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-21 17:13:05 +0200

    Programma completo senza bonus

--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -20,14 +20,16 @@ for(int i = 0; i < numeroAppuntamenti; i++)
     Console.WriteLine("inserisci l'appuntamento numero " + (i+1));
     Console.WriteLine("Inserisci il nome della persona con cui hai l'appuntamento: ");
     string? nomeAppuntamento = Console.ReadLine();
+    
     DateTime dataAttuale = DateTime.Now;
-  
-    Console.WriteLine("Inserisci la data dell'appuntamento: ");
-    DateTime dataPerAppuntamento = DateTime.Parse(Console.ReadLine());
-   if (dataPerAppuntamento < dataAttuale)
-   {
+    DateTime dataPerAppuntamento;
+    do
+    {
+        Console.WriteLine("Inserisci la data dell'appuntamento: ");
+        dataPerAppuntamento = DateTime.Parse(Console.ReadLine());
+    } while (dataPerAppuntamento < dataAttuale);
 
-   }
+
     Console.WriteLine("Inserisci il luogo dell'appuntamento: ");
     string? luogoAppuntamento = Console.ReadLine();
 
