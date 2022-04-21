7d54c1e2813a641e5046dfcb383298e50782f42d
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-21 14:32:48 +0200

    Aggiunto metodo, costruttore ed eccezione

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento
+    {
+        private string? nome;
+        public DateTime dataEOraAttuale = DateTime.Now;
+        public DateTime dataEOraAppuntamento;
+        public string? localitaAppuntamento;
+
+        public Appuntamento(string nome, DateTime dataEOraAppuntamento, string localitaAppuntamento)
+        {
+            this.nome = nome;
+            this.localitaAppuntamento = localitaAppuntamento;
+            this.dataEOraAppuntamento = dataEOraAppuntamento;
+
+            if (dataEOraAppuntamento < dataEOraAttuale)
+            {
+                throw new ArgumentOutOfRangeException("dataEOraAppuntamento", "non puo essere una data del passato ");
+            }
+        }
+     public DateTime cambioAppuntamnto()
+        {
+            DateTime cambioDellAppuntamento = DateTime.Parse(Console.ReadLine());
+            this.dataEOraAppuntamento = cambioDellAppuntamento;
+            if (dataEOraAppuntamento < dataEOraAttuale)
+            {
+                throw new ArgumentOutOfRangeException("dataEOraAppuntamento", "Non puo essere una data del passato ");
+            }
+            return dataEOraAppuntamento;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public string GetLocalitaAppuntamento()
+        {
+            return localitaAppuntamento;
+        }
+    }
+}
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ListaAppuntamenti;
+try
+{
+    Appuntamento dottore = new Appuntamento("Dott. Esposito", DateTime.Parse("22/04/2022"), "Napoli");
+}catch(ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Mi dispiace ma " + e.ParamName + " ha detto " + e.Message);
+}
